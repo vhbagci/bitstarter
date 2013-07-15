50986d116213a2a8d6a7d34265bbee1faf438f1b
Volkan Bagci <bagciv@hotmail.com>
2013-07-15 20:32:24 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync('index.html');
+var str = buf.toString('utf-8',0,buf.length-1);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
